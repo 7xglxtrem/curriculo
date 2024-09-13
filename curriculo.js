56036bc261bdc1d75ce7b5d0a7b6ef6376e6559a
7xglxtrem <126262446+7xglxtrem@users.noreply.github.com>
2024-09-13 16:27:32 -0300

    Create curriculo.js

---
+++ curriculo.js
@@ -0,0 +1,6 @@
+// Adicione aqui qualquer funcionalidade JavaScript que desejar
+// Exemplo: Um alerta quando a página carrega
+
+window.onload = function() {
+    alert('Bem-vindo ao meu currículo!');
+};
